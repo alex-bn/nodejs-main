e9d5e6609bca92f6d0d76479df51e0a3461030c2
Alex Bajan <alex.i.bajan@gmail.com>
2022-01-25 08:02:09 +0200

    simple nested POST route

--- 7 - Section 11 - Data Modeling & Advanced Mongoose/controllers/reviewController.js
+++ 7 - Section 11 - Data Modeling & Advanced Mongoose/controllers/reviewController.js
@@ -15,8 +15,12 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req, res, next) => {
-  const newReview = await Review.create(req.body);
+  // allow nested routes // when they are not specified in the request body
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
 
+  //
+  const newReview = await Review.create(req.body);
   res.status(201).json({
     status: 'Success',
     data: {
