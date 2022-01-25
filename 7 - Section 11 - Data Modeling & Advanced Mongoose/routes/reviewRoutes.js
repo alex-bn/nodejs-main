8e8e4f9370fc4a01162a016ea905918bb4ceb75a
Alex Bajan <alex.i.bajan@gmail.com>
2022-01-25 08:53:16 +0200

    nested routes, GET endpoint

--- 7 - Section 11 - Data Modeling & Advanced Mongoose/routes/reviewRoutes.js
+++ 7 - Section 11 - Data Modeling & Advanced Mongoose/routes/reviewRoutes.js
@@ -5,6 +5,7 @@ const authController = require('../controllers/authController');
 // by default each router only has access to their specific routes, so in this this case mergeParams will give us access to the tourId param
 const router = express.Router({ mergeParams: true });
 // POST /tour/23nba87d/reviews -> will end up in the handler function below
+// POST /tour/23nba87d/reviews
 
 router
   .route('/')
