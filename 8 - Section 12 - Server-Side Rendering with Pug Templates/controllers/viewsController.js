a98ab5953c151195457014878ebdbd595350bcf8
Alex Bajan <alex.i.bajan@gmail.com>
2022-02-06 17:10:26 +0200

    pug: building the tour page

--- 8 - Section 12 - Server-Side Rendering with Pug Templates/controllers/viewsController.js
+++ 8 - Section 12 - Server-Side Rendering with Pug Templates/controllers/viewsController.js
@@ -14,8 +14,18 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) get the data for the requested tour (including reviews and tour guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  // 2) build template
+
+  // 3) render template using data from step 1)
+
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour: tour,
   });
-};
+});
