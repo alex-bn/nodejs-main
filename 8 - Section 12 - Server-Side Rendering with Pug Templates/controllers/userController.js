40d4c5ea1335c3fa3078498418142b121eab403a
Alex Bajan <alex.i.bajan@gmail.com>
2022-02-17 07:03:25 +0200

    uploading multiple files

--- 8 - Section 12 - Server-Side Rendering with Pug Templates/controllers/userController.js
+++ 8 - Section 12 - Server-Side Rendering with Pug Templates/controllers/userController.js
@@ -19,7 +19,7 @@ const factory = require('./handlerFactory');
 // store image into memory
 const multerStorage = multer.memoryStorage();
 
-// filer file
+// filter file
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
     cb(null, true);
@@ -36,19 +36,19 @@ const upload = multer({
 exports.uploadUserPhoto = upload.single('photo');
 
 // resize and convert images
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
     .toFile(`public/img/users/${req.file.filename}`);
 
   next();
-};
+});
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
